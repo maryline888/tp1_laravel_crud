04037d0bb2798e8c15053ee3bab328c877a2cbd1
maryline888 <maryline888@gmail.com>
2023-05-29 12:25:39 -0400

    ajout de la fx create etudiant

--- app/Http/Controllers/EtudiantsController.php
+++ app/Http/Controllers/EtudiantsController.php
@@ -20,4 +20,27 @@ class EtudiantsController extends Controller
         $villes = Villes::all();
         return view('etudiants.create', ['villes' => $villes]);
     }
+
+    public function store(Request $request)
+    {
+        $newEtudiant = Etudiants::create([
+            'last_name' => $request->last_name,
+            'first_name' => $request->first_name,
+            'address' => $request->address,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'date_naissance' => $request->date_naissance,
+            'ville_id' => $request->ville_id
+
+        ]);
+
+        return redirect(route('etudiants.show', $newEtudiant->id));
+    }
+
+    public function show()
+    {
+
+        $etudiants = Etudiants::all();
+        return view('etudiants.index', ['etudiants' => $etudiants]);
+    }
 }
